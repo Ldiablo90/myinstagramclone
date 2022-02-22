f7e20035072077af1267756b407138962f8d737a
Ldiablo90 <ldiablo90@gmail.com>
2022-02-22 18:57:19 +0900

    조금

--- redux/actions/index.js
+++ redux/actions/index.js
@@ -1,4 +1,4 @@
-import { USER_STATE_CHANGE, USER_POSTS_STATE_CHANGE, USER_FOLLOWING_STATE_CHANGE } from '../constants'
+import { USER_STATE_CHANGE, USER_POSTS_STATE_CHANGE, USER_FOLLOWING_STATE_CHANGE, USERS_DATA_STATE_CHANGE } from '../constants'
 
 import { fireAuth, fireStore } from '../../firebase'
 
@@ -57,6 +57,47 @@ export function fetchUserFollowing() {
         dispatch({
             type: USER_FOLLOWING_STATE_CHANGE,
             following : following
+        })
+    })
+}
+
+export function fetchUserData(uid) {
+    const outDoc = doc(db, 'users', uid)
+    return (async (dispatch, getState) => {
+        const found = getState().usersState.users.some(el => el.uid === uid);
+        if(!found){
+            const getSnapshot = await getDoc(outDoc)
+
+            if(getSnapshot.exists){
+                let user = getSnapshot.data();
+                user.uid = getSnapshot.id;
+                dispatch({
+                    type: USERS_DATA_STATE_CHANGE,
+                    user
+                })
+            }
+        }
+    })
+}
+
+export function fetchUsersFollowingPosts(uid) {
+    const outDoc = doc(db, 'posts', auth.currentUser.uid)
+    const innerDoc = collection(outDoc, 'userPosts');
+    const orderby = query(innerDoc, orderBy('creation'))
+    let posts;
+    return (async (dispatch) => {
+        const getSnapshot = await getDocs(orderby)
+        const uid = getSnapshot.query.EP.path.segments[1]
+        !getSnapshot.empty ?
+            posts = getSnapshot.docs.map(doc => {
+                const data = doc.data();
+                const id = doc.id;
+                return { id, ...data }
+            })
+            : console.log('does empty');
+        dispatch({
+            type: USER_POSTS_STATE_CHANGE,
+            posts : posts
         })
     })
 }
